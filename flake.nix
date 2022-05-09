5f079cfc0c719eb72791b4d51be5385516f67336
hugosenari <hugosenari@gmail.com>
2022-05-09 20:35:32 -0300

    chore: re index bencode

--- flake.nix
+++ flake.nix
@@ -7,14 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."bencode-master".dir   = "master";
-  inputs."bencode-master".owner = "nim-nix-pkgs";
-  inputs."bencode-master".ref   = "master";
-  inputs."bencode-master".repo  = "bencode";
-  inputs."bencode-master".type  = "github";
-  inputs."bencode-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."bencode-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
